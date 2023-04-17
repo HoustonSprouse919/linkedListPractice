64e3c93aced0fbe74c48242fa09ec1641dc63b87
HoustonSprouse919 <89546823+HoustonSprouse919@users.noreply.github.com>
2023-04-17 10:34:39 -0400

    Update javascript.js
    added size and prepend

--- javascript.js
+++ javascript.js
@@ -26,10 +26,31 @@ function linkedList() {
           tail.next = newNode;
       }
     },
-
-    } 
+    prepend(value){
+let newNode = createNode(value,this.head);
+this.head = newNode;
+    }, 
+    size(){
+        let sizeNum =0;
+        if(this.head == null){
+            return sizeNum;
+        } else{
+            sizeNum =1;
+            let tail = this.head;
+          while(tail.next != null){
+              sizeNum+=1;
+              tail = tail.next;
+        }
+        console.log("the list is " + sizeNum + " long");
+        }
+    },
+}
 }
 let list = linkedList();
 list.append(2);
 list.append(3);
+list.prepend(55);
 console.log(list);
+console.log(list.head.next);
+console.log(list.head.next.next);
+list.size()
